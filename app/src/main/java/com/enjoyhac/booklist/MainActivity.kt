3137d91338eb0da6e6df731e9eecef597754beca
Hagi T <marspeoplehg@gmail.com>
2025-01-12 20:00:36 +0400

    209. Saving a User to Firestore Database - Test User

--- app/src/main/java/com/enjoyhac/booklist/MainActivity.kt
+++ app/src/main/java/com/enjoyhac/booklist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.enjoyhac.booklist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +12,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.enjoyhac.booklist.ui.theme.BooklistTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BooklistTheme {
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "XXX"
+                user["lastName"] = "YYYY"
+
+                db.collection("users").add(user)
+                    .addOnSuccessListener {
+                        Log.d("FB", "onCreate: ${it.id}")
+                    }
+                    .addOnFailureListener {
+                        Log.e("FB", "Error adding user", it)
+                    }
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
