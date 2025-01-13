72dda9a09f34044ff95a85c87eb39d2227e8fdb2
Hagi T <marspeoplehg@gmail.com>
2025-01-13 17:08:50 +0400

    234

--- app/src/main/java/com/enjoyhac/booklist/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/enjoyhac/booklist/screens/home/ReaderHomeScreen.kt
@@ -1,25 +1,28 @@
 package com.enjoyhac.booklist.screens.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.enjoyhac.booklist.screens.ReaderScreens
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun Home(navController: NavController) {
@@ -56,17 +59,27 @@ fun ReaderAppBar(
                         imageVector = Icons.Default.Favorite,
                         contentDescription = "Logo Icon",
                         modifier = Modifier.clip(RoundedCornerShape(12.dp))
-                                           .scale(0.1f)
+                                           .scale(0.7f)
                     )
                     Text(
                         text = title,
                         color = Color.Red.copy(alpha = 0.7f),
                         style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
                     )
+                    Spacer(modifier = Modifier.width(150.dp))
                 }
             }
         },
-        actions = {},
+        actions = {
+                  IconButton(onClick = {
+                      FirebaseAuth.getInstance().signOut().run {
+                          navController.navigate(ReaderScreens.LoginScreen.name)
+                      }
+                  }) {
+                      Icon(imageVector = Icons.Filled.Logout,
+                      contentDescription = "Logout")
+                  }
+        },
         backgroundColor = Color.Transparent, elevation = 0.dp
     )
 }
