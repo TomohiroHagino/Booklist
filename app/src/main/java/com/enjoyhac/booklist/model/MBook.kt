1fc6d5b9c520a557e91353a60fe8352d404d0ca3
Hagi T <marspeoplehg@gmail.com>
2025-01-13 18:13:09 +0400

    235. Adding the TopReading Section

---
+++ app/src/main/java/com/enjoyhac/booklist/model/MBook.kt
@@ -0,0 +1,8 @@
+package com.enjoyhac.booklist.model
+
+data class MBook(
+    var id: String? = null,
+    var title: String? = null,
+    var authors: String? = null,
+    var notes: String? = null,
+)
--- app/src/main/java/com/enjoyhac/booklist/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/enjoyhac/booklist/screens/home/ReaderHomeScreen.kt
@@ -1,9 +1,7 @@
 package com.enjoyhac.booklist.screens.home
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.animation.AnimatedContentScope.SlideDirection.Companion.Left
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -16,16 +14,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.enjoyhac.booklist.model.MBook
 import com.enjoyhac.booklist.screens.ReaderScreens
 import com.google.firebase.auth.FirebaseAuth
 
+@Preview
 @Composable
-fun Home(navController: NavController) {
+fun Home(navController: NavController = NavController(LocalContext.current)) {
     Scaffold(
         topBar = {
                  ReaderAppBar(title = "Booklist", navController = navController )
@@ -39,6 +43,7 @@ fun Home(navController: NavController) {
         //content
         Surface(modifier = Modifier.fillMaxSize()) {
             //home content
+            HomeContect(navController)
         }
     }
 }
@@ -58,8 +63,9 @@ fun ReaderAppBar(
                     Icon(
                         imageVector = Icons.Default.Favorite,
                         contentDescription = "Logo Icon",
-                        modifier = Modifier.clip(RoundedCornerShape(12.dp))
-                                           .scale(0.7f)
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(12.dp))
+                            .scale(0.7f)
                     )
                     Text(
                         text = title,
@@ -76,12 +82,49 @@ fun ReaderAppBar(
                           navController.navigate(ReaderScreens.LoginScreen.name)
                       }
                   }) {
-                      Icon(imageVector = Icons.Filled.Logout,
-                      contentDescription = "Logout")
+                      Icon(
+                          imageVector = Icons.Filled.Logout,
+                          contentDescription = "Logout",
+                          tint = Color.Green.copy(alpha = 0.4f)
+                      )
                   }
         },
         backgroundColor = Color.Transparent, elevation = 0.dp
     )
+}
+
+@Composable
+fun HomeContect(navController: NavController) {
+    Column(Modifier.padding(2.dp),
+        verticalArrangement = Arrangement.SpaceEvenly){
+        Row(modifier = Modifier.align(alignment = Alignment.Start)) {
+            TitleSection(label = "あなたは現在、読書中です。。。")
+        }
+    }
+}
+
+
+@Composable
+fun TitleSection(modifier: Modifier = Modifier, label: String) {
+    Surface(
+        modifier = modifier.padding(start = 5.dp, top = 1.dp),
+        color = Color.LightGray
+    ) {
+        Column {
+            Text(
+                text = label,
+                fontSize = 19.sp,
+                fontStyle = FontStyle.Normal,
+                textAlign = TextAlign.Left
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ReadingRightNowArea(books: List<MBook>, navController: NavController) {
+
 }
 
 @Composable
