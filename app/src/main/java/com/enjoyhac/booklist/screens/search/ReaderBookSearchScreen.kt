df063d352a7d01e64aa4a33d5e1ce8cf92ae77e8
Hagi T <marspeoplehg@gmail.com>
2025-01-14 21:30:58 +0400

    245 finished

--- app/src/main/java/com/enjoyhac/booklist/screens/search/ReaderBookSearchScreen.kt
+++ app/src/main/java/com/enjoyhac/booklist/screens/search/ReaderBookSearchScreen.kt
@@ -1,12 +1,26 @@
 package com.enjoyhac.booklist.screens.search
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ExperimentalComposeApi
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.enjoyhac.booklist.components.InputField
 import com.enjoyhac.booklist.components.ReaderAppBar
 import com.enjoyhac.booklist.screens.ReaderScreens
 
@@ -20,6 +34,46 @@ fun SearchScreen(navController: NavController) {
             navController = navController,
         ) { navController.navigate(ReaderScreens.ReaderHomeScreen.name) }
     }) {
-
+        Surface() {
+            Column {
+                SearchForm(
+                    modifier = Modifier.fillMaxWidth()
+                                       .padding(16.dp)
+                )
+            }
+        }
     }
+}
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SearchForm(
+    modifier: Modifier = Modifier,
+    loading: Boolean = false,
+    hint: String = "Search",
+    onSearch: (String) -> Unit = {}
+) {
+    Column() {
+        val searchQueryState = rememberSaveable {
+            mutableStateOf("")
+        }
+        val keyboardController = LocalSoftwareKeyboardController.current
+        val valid = remember(searchQueryState.value) {
+            searchQueryState.value.trim().isNotEmpty()
+        }
+
+        InputField(
+            valueState = searchQueryState,
+            labelId = "Search",
+            enabled = true,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                    onSearch(searchQueryState.value.trim())
+                    searchQueryState.value = ""
+                    keyboardController?.hide()
+            }
+        )
+    }
+
 }
